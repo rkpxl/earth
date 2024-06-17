cdd991e7045c5b184f49725908f7ed2a5413a104
Ritikjainkkar <ritikjainkkar@gmail.com>
2024-06-18 00:41:35 +0530

    fix: loading

--- src/Scenes/Login/Login.tsx
+++ src/Scenes/Login/Login.tsx
@@ -124,10 +124,6 @@ const Login = () => {
     dispatch(showMessage({ message: 'Please refer to email and password', severity: 'warning' }))
   }
 
-  if (isLoading) {
-    return <CircularProgress />
-  }
-
   return (
     <Suspense fallback={<CircularProgress />}>
       <Head>
